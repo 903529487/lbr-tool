b015ec1e947344d2bdd15aea65c0c1c017039ffa
lll <2222772@163.com>
2023-01-11 12:00:27 +0800

    feat(all): 新增生成uuid

---
+++ src/tool/generateUuid.ts
@@ -0,0 +1,14 @@
+
+/**
+ * @title 生成uuid
+ * @return string
+ */
+const generateUuid = () => {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        const r = (Math.random() * 16) | 0,
+            v = c == 'x' ? r : (r & 0x3) | 0x8;
+        return v.toString(16);
+    });
+};
+
+export default generateUuid
